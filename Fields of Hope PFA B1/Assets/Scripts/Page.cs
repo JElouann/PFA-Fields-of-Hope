e26aae1b73aec3ae360c20882341f92e5625abb7
Elouann <elouann.jouenne@e-artsup.net>
2024-04-12 16:02:36 +0200

    Refactoring book system

---
+++ Fields of Hope PFA B1/Assets/Scripts/Page.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Page : MonoBehaviour
+{
+    private int _dayCounter;
+    [SerializeField] private List<string> _paragraphes;
+
+    private TextMeshProUGUI _currentContent;
+}
