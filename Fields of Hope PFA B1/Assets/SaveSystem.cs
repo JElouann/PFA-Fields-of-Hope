eb172cab64776b2b7164064e91ca003173fd5135
Theo160905 <145351781+Theo160905@users.noreply.github.com>
2024-05-22 11:30:58 +0200

    PTET CA MARCHE

--- Fields of Hope PFA B1/Assets/SaveSystem.cs
+++ Fields of Hope PFA B1/Assets/SaveSystem.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Unity.VisualScripting;
 
 public class SaveSystem : MonoBehaviour
 {
-    public TMP_InputField inputField;
+    //public TMP_InputField inputField;
 
     [SerializeField]
     private StatsManager statsManager;
@@ -17,7 +18,9 @@ public class SaveSystem : MonoBehaviour
     private int Life;
     private int Hunger;
     private int Seeds;
+    private int food;
 
+    /*
     private int carotte;
     private int betterave;
     private int poireau;
@@ -26,25 +29,71 @@ public class SaveSystem : MonoBehaviour
     private int rutabaga;
     private int topinambour;
     private int radis;
+    */
 
-    private void Awake()
-    {
+    public TextMeshProUGUI dayTMP;
+    public TextMeshProUGUI LifeTMP;
+    public TextMeshProUGUI HungerTMP;
+    public TextMeshProUGUI SeedsTMP;
+    public TextMeshProUGUI foodTMP;
 
-    }
+    /*
+    private int carotteTMP;
+    private int betteraveTMP;
+    private int poireauTMP;
+    private int potironTMP;
+    private int pommedeterreTMP;
+    private int rutabagaTMP;
+    private int topinambourTMP;
+    private int radisTMP;
+    */
+
     public void SaveData()
     {
-        PlayerPrefs.SetString("Input", inputField.text);
+        //PlayerPrefs.SetString("Input", inputField.text);
+        day = dayManager._dayCounter;
+        Life = statsManager.Life;
+        Hunger = statsManager.Hunger;
+        Seeds = statsManager.Seeds;
+        food = statsManager.Food;
+
+
+        /*
+        carotte = statsManager.Carotte;
+        betterave = statsManager.Beterave;
+        poireau = statsManager.Poireau;
+        potiron = statsManager.Potiron;
+        pommedeterre = statsManager.Patate;
+        rutabaga = statsManager.Rutabaga;
+        topinambour = statsManager.Topinambour;
+        radis = statsManager.Radis;
+        */
         Debug.Log("SAVE");
     }
 
     public void LoadData()
     {
-        inputField.text = PlayerPrefs.GetString("Input");
+        //inputField.text = PlayerPrefs.GetString("Input");
+        dayTMP.text = day.ToString();
+        LifeTMP.text = Life.ToString();
+        HungerTMP.text = Hunger.ToString();
+        SeedsTMP.text = Seeds.ToString();
+        foodTMP.text = food.ToString();
+        /*dayManager._dayCounter = day;
+        statsManager.Life = Life;
+        statsManager.Hunger = Hunger;
+        statsManager.Seeds = Seeds;
+        statsManager.Food = food;*/
         Debug.Log("LOAD");
     }
 
     public void DeleteData()
     {
-        inputField.text = string.Empty;
+        //inputField.text = string.Empty;
+        dayTMP = null;
+        LifeTMP = null;
+        HungerTMP = null;
+        SeedsTMP = null;
+        foodTMP = null;
     }
 }
