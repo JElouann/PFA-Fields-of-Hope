ed34e84503698add762735da87510e8a84819344
Theo160905 <145351781+Theo160905@users.noreply.github.com>
2024-06-13 11:46:38 +0200

    👈⦑Ỡ‿Ỡ⦒👉️

--- Fields of Hope PFA B1/Assets/Scripts/ChangeScene.cs
+++ Fields of Hope PFA B1/Assets/Scripts/ChangeScene.cs
@@ -1,15 +1,29 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
-    public void SceneChanged(string NameOfScene)
+    [SerializeField]
+    private Animator animator;
+
+    public void SceneChanged(string name)
     {
-        SceneManager.LoadScene(NameOfScene);
+        StartCoroutine(Anim(name));
     }
 
     public void Quit()
     {
         Application.Quit();
+    }
+
+    private IEnumerator Anim(string NameOfScene)
+    {
+        Debug.Log(animator.GetFloat("POPILOPO"));
+        animator.SetFloat("POPILOPO", 1);
+        Debug.Log(animator.GetFloat("POPILOPO"));
+        yield return new WaitForSecondsRealtime(2f);
+        SceneManager.LoadScene(NameOfScene);
+        animator.SetFloat("LOL", 0);
     }
 }
